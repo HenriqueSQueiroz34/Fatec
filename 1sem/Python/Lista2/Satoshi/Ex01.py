7df00715aa008a0b5d7ebf439aebb2289ffc8469
HenriqueSQueiroz34 <henrique.queiroz.14.contato@gmail.com>
2026-05-23 21:35:52 -0300

    Lista2/Satoshi/Ex01.py

---
+++ 1sem/Python/Lista2/Satoshi/Ex01.py
@@ -0,0 +1,17 @@
+import random
+vetor:int = [0]*50
+somai:int = 0
+j:int
+contm:int = 0
+med:int = 0
+
+for j in range(50):
+    vetor[j] = random.randint(1,300) # int(input(f'Digite o termo {j+1}:'))
+    if (vetor[j]%2==1):
+        somai += vetor[j]
+    if (vetor[j] >= 10 and vetor[j] <= 200):
+        med += vetor[j]
+        contm += 1
+
+print(f'Soma dos valores impares: {somai}')
+print(f'Média dos valores entre 10 e 200: {med/contm:.2f}')
