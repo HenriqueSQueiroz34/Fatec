cbf409cba9c1f6517c2feb8f75e72a9b0e9100a9
HenriqueSQueiroz34 <henrique.queiroz.14.contato@gmail.com>
2026-06-17 22:06:24 -0300

    UPDT 1sem/Python/Semaforo/Lista1/Ex01.py Remodelação das variáveis

--- 1sem/Python/Semaforo/Lista1/Ex01.py
+++ 1sem/Python/Semaforo/Lista1/Ex01.py
@@ -1,61 +1,47 @@
 import time
 import multiprocessing
+import random
 
 sentido: str = ['azul','amarelo','vermelho','verde']
 semaforo = None
-limite: int
 andado: int
-cont: int
 
-def carregar(s,l,a,c):
+def carregar(s,a):
     global semaforo
-    global limite
     global andado
     global cont
     semaforo = s
-    limite = l
     andado = a
-    cont = c
 
 def thread(i):
     global sentido
     global semaforo
-    global limite
     global andado
-    global cont
+    lim = 30
 
     with semaforo:
-        while (limite.value > 0):
-            limite.value -= andado.value
-            lim = andado.value * cont.value
-            print('Carro',sentido[i],'andou',lim,'m')
+        while (lim > 0):
+            lim -= andado.value
+            print('Carro',sentido[i],'andou',andado.value,'m')
+            andado.value = random.randint(1,5)
             time.sleep(0.5)
-            if (limite.value == 0):
+            if (lim <= 0):
                 print('Venha próximo carro\n')
-            cont.value += 1
-            
-        limite.value = 30
-        andado.value = 5
-        cont.value = 1
 
 def main():
     i: int
-    limite:int
     andado:int
     sem = None
-    cont:int
     params:int = [0]*4
 
-    limite = multiprocessing.Value('i',30)
-    andado = multiprocessing.Value('i',5)
-    cont = multiprocessing.Value('i',1)
+    andado = multiprocessing.Value('i',random.randint(1,5))
 
     for i in range(len(params)):
         params[i] = i
 
     with multiprocessing.Manager() as manager:
         sem = manager.Semaphore(1)
-        with multiprocessing.Pool(processes=4,initializer=carregar,initargs=(sem,limite,andado,cont)) as pool:
+        with multiprocessing.Pool(processes=4,initializer=carregar,initargs=(sem,andado,cont)) as pool:
             pool.map(thread,params)
 
     print('Todos os carros passaram!!')
