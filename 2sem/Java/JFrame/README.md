6839dfe6599996c6af3af864b567399d9cc5db7b
HenriqueSQueiroz34 <henrique.queiroz.14.contato@gmail.com>
2026-09-20 18:00:29 -0300

    UPDT /Java/[JFrame/][Lista2/]

---
+++ 2sem/Java/JFrame/README.md
@@ -0,0 +1,5 @@
+Ex01: Receba 3 coeficientes A, B, e C de uma equação do 2º grau da fórmula AX²+BX+C=0. Verifique e mostre a existência de raízes reais e se caso exista, calcule e mostre.
+
+Ex02: Receba um número inteiro. Calcule e mostre o seu fatorial.
+
+Ex03: Receba um número. Calcule e mostre os resultados da tabuada desse número.
--- 2sem/Java/Lista 2/prj_java17-09-2026/src/Ex12.java
+++ 2sem/Java/Lista 2/prj_java17-09-2026/src/Ex12.java
@@ -1,3 +1,37 @@
+import javax.swing.JOptionPane;
 public class Ex12 {
+    public static void main (String args[]){
+        int pontos[] = new int[]{0,1,5,3,3,9,0,0};
+        int tabul[][] = new int[8][8];
+        int soma = 0;
+        int peca = 0;
+        
+        for (int l=0;l<8;l++){
+            for (int c=0;c<8;c++){
+                
+                do {
+                    peca = Integer.parseInt(JOptionPane.showInputDialog(
+                        "Escolha entre as opções abaixo para as peças do tabuleiro:\n"
+                        + "1- Peão\n2- Torre\n3- Bispo\n4- Cavalo\n5- Rainha\n6- Rei\n7- Vazio\n"
+                        + "Dê o código da peça na posição ["+(l+1)+"]["+(c+1)+"]:"
+                    ));
+                } while (peca < 1 || peca > 7);
+                
+                tabul[l][c] = peca;
+                soma += pontos[peca];
+            }
+        }
+        
+        System.out.println("Soma das peças: "+soma);
+        Tabuleiro(tabul);
+    }
     
+    static void Tabuleiro (int t[][]){
+        for (int l=0;l<8;l++){
+            for (int c=0;c<8;c++){
+                System.out.print(t[l][c] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
